b9828e3ab2bc228686ea9f6e117ff0f05d335f2d
GABRIEL VASCONCELOS VALE <gabriel.vale@alunos.sc.senac.br>
2022-04-29 19:55:08 -0400

    Exercicio

---
+++ AnoBissexto/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace AnoBissexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("Digite um ano para calcularmos se é ou não Bissexto, ex: 2022 . ");
+
+            int ano = int.Parse(Console.ReadLine());
+
+
+
+
+
+
+            
+           
+            if ((ano % 400 == 0) && (ano % 4 == 0)) // bissexto
+            {
+                Console.WriteLine("Esse ano é Bissexto.");
+
+            }else if(ano %400 == 0)
+            {
+                Console.WriteLine(" Esse ano  é bissexto.");
+            }
+            else if (ano %4==0 && ano %100 != 0)
+            {
+                Console.WriteLine("Esse ano é bissexto");
+            }
+            else
+            {
+                Console.WriteLine(" Esse ano não é bissexto");
+                   
+            }
+            
+        }
+    }
+}
